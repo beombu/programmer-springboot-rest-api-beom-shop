5df00c52271a929b3c3fa338108d9156b1999cb9
beombu <beombu13@gmail.com>
2023-07-20 04:39:53 +0900

    init progject

---
+++ src/main/java/com/example/beomshop/BeomShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.beomshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeomShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeomShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beomshop/BeomShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beomshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeomShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
